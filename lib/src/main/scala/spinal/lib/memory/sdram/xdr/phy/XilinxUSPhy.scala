d2218870ae51d81322dd96cdaee0b8c24963ea2c
athxrva-0209 <atharvawadnere3@gmail.com>
2025-08-12 01:30:39 +0530

    modified PHY

--- lib/src/main/scala/spinal/lib/memory/sdram/xdr/phy/XilinxUSPhy.scala
+++ lib/src/main/scala/spinal/lib/memory/sdram/xdr/phy/XilinxUSPhy.scala
@@ -2,22 +2,16 @@ package spinal.lib.memory.sdram.xdr.phy
 
 import spinal.core._
 import spinal.lib._
-import spinal.lib.blackbox.xilinx.ultrascale.{IDELAYCTRL, IDELAYE3, IOBUF, IOBUFDS, ISERDESE3, MMCME3_ADV, OBUFDS, ODELAYE3, OSERDESE3}
+import spinal.lib.blackbox.xilinx.ultrascale._
 import spinal.lib.bus.misc.BusSlaveFactory
 import spinal.lib.memory.sdram.SdramLayout
 import spinal.lib.memory.sdram.xdr.{PhyLayout, SdramXdrIo, SdramXdrPhyCtrl}
-
-import scala.collection.Seq
 import spinal.core.sim._
-import spinal.lib.memory.sdram.SdramGeneration.DDR3
 import spinal.lib.memory.sdram._
 
-import scala.util.control.Breaks.{break, breakable}
 
-
-
-object XilinxUSPhy{
-  def phyLayout(sl : SdramLayout, clkRatio : Int) = PhyLayout(
+object XilinxUSPhy {
+  def phyLayout(sl: SdramLayout, clkRatio: Int) = PhyLayout(
     sdram = sl,
     phaseCount = clkRatio,
     dataRate = 2,
@@ -29,133 +23,111 @@ object XilinxUSPhy{
   )
 }
 
-case class XilinxUSPhy(sl : SdramLayout,
-                       clkRatio : Int,
-                       clk90 : ClockDomain,
-                       serdesClk0 : ClockDomain,
-                       serdesClk90 : ClockDomain) extends Component {
-  val pl = XilinxUSPhy.phyLayout(sl, clkRatio)
+case class XilinxUSPhy(sl: SdramLayout,
+                       clkRatio: Int,
+                       clk90: ClockDomain,
+                       serdesClk0: ClockDomain,
+                       serdesClk90: ClockDomain) extends Component {
 
+  val pl = XilinxUSPhy.phyLayout(sl, clkRatio)
   val io = new Bundle {
-    val ctrl = slave(SdramXdrPhyCtrl(pl))
+    val ctrl  = slave(SdramXdrPhyCtrl(pl))
     val sdram = master(SdramXdrIo(sl))
-    val debug = new Bundle {
-      val dqsEnableWindow = out Bits(4 bits)
-      val dqsT = out Bits(((sl.dataWidth + 7) / 8) bits)
-      val dqT = out Bits(sl.dataWidth bits)
-    }
   }
 
   assert(clkRatio == 2)
   val phaseCount = clkRatio
 
   val clk270Rst = ResetCtrl.asyncAssertSyncDeassert(
-    input = ClockDomain.current.isResetActive,
-    clockDomain = clk90.withRevertedClockEdge(),
+    input         = ClockDomain.current.isResetActive,
+    clockDomain   = clk90.withRevertedClockEdge(),
     inputPolarity = HIGH,
-    outputPolarity = HIGH
+    outputPolarity= HIGH
   )
   val clk270 = clk90.withRevertedClockEdge().copy(reset = clk270Rst)
 
   val clk90Rst = ResetCtrl.asyncAssertSyncDeassert(
-    input = clk270Rst,
-    clockDomain = clk90,
+    input         = clk270Rst,
+    clockDomain   = clk90,
     inputPolarity = HIGH,
-    outputPolarity = HIGH
+    outputPolarity= HIGH
   )
 
-  //  clk270.setSynchronousWith(ClockDomain.current)
-
   val idelayctrl = IDELAYCTRL()
-  idelayctrl.REFCLK := serdesClk90.readClockWire //TODO serdesClk90
-  idelayctrl.RST := ClockDomain.isResetActive
+  idelayctrl.REFCLK := serdesClk90.readClockWire
+  idelayctrl.RST    := ClockDomain.isResetActive
 
-  def valueToOutput(name: String, phase: Bool): Bool = seqToOutput(name, Seq.fill(4 * 2)(phase))._1
+  def seqToOutput(name: String, seq: Seq[Bool], outputEnable: Seq[Bool], phase90: Boolean = false): (Bool, Bool) = {
+    val oser = OSERDESE3(DATA_WIDTH = phaseCount*2)
+    val dataBits = Bits(8 bits)
+    for (i <- 0 until phaseCount*2) dataBits(i) := seq(i)
+    for (i <- phaseCount*2 until 8) dataBits(i) := False
+    oser.D := dataBits
 
-  def sdrToOutput(name: String, phases: Seq[Bool], outputEnable: Seq[Bool] = List.fill(phaseCount * 2)(True)): Bool = seqToOutput(name, phases.map(p => Seq.fill(2)(p)).flatten, outputEnable)._1
-
-  def ddrToOutput(name: String, phases: Seq[Seq[Bool]], outputEnable: Seq[Bool] = List.fill(phaseCount * 2)(True)): Bool = seqToOutput(name, phases.flatten, outputEnable)._1
-
-  def seqToOutput(name: String, seq: Seq[Bool], outputEnable: Seq[Bool] = List.fill(phaseCount * 2)(True), phase90: Boolean = false): (Bool, Bool) = {
-    val serdes = OSERDESE3(
-      DATA_WIDTH = phaseCount * 2,
-      INIT = false,
-      ODDR_MODE = "TRUE" // Set to TRUE to enable DDR mode.
-    ).setName(s"${name}_OSERDESE3")
-
-    // Concatenate the input sequence and assign it to the 8-bit D port.
-    // Any unused bits of the D port will be padded with False.
-    val dataWidth = phaseCount * 2
-    serdes.D := (B(seq).resize(dataWidth) ## B(0, 8 - dataWidth bits)).resize(8)
-
-    phase90 match {
-      case false =>
-        serdes.CLK := serdesClk0.readClockWire
-        serdes.CLKDIV := ClockDomain.current.readClockWire
-        serdes.RST := ClockDomain.current.isResetActive
-      case true =>
-        serdes.CLK := serdesClk90.readClockWire
-        serdes.CLKDIV := clk90.readClockWire
-        serdes.RST := clk90Rst
+    if (!phase90) {
+      oser.CLK    := serdesClk0.readClockWire
+      oser.CLKDIV := ClockDomain.current.readClockWire
+      oser.RST    := ClockDomain.current.isResetActive
+    } else {
+      oser.CLK    := serdesClk90.readClockWire
+      oser.CLKDIV := clk90.readClockWire
+      oser.RST    := clk90Rst
     }
 
-    // OSERDESE3 has a single T input, so we use a logical OR of all `outputEnable` signals.
-    serdes.T := !outputEnable.reduce(_ & _)
-    (serdes.OQ, serdes.T_OUT) // Use T_OUT as the tristate output
+    // Single tri-state control
+    oser.T := !outputEnable.orR
+    (oser.OQ, oser.T_OUT)
   }
 
+  def valueToOutput(name: String, phase: Bool) = seqToOutput(name, Seq.fill(4*2)(phase), Seq.fill(4*2)(True))._1
+  def sdrToOutput(name: String, phases: Seq[Bool]) = seqToOutput(name, phases.flatMap(p => Seq.fill(2)(p)), Seq.fill(phaseCount*2)(True))._1
+  def ddrToOutput(name: String, phases: Seq[Seq[Bool]]) = seqToOutput(name, phases.flatten, Seq.fill(phaseCount*2)(True))._1
+
   val clkBuf = OBUFDS()
-  clkBuf.I := seqToOutput("CK", Seq.fill(phaseCount)(Seq(True, False)).flatten, phase90 = true)._1
-  io.sdram.CK := clkBuf.O
+  clkBuf.I := seqToOutput("CK", Seq.fill(phaseCount)(Seq(True, False)).flatten, Seq.fill(phaseCount*2)(True), phase90 = true)._1
+  io.sdram.CK  := clkBuf.O
   io.sdram.CKn := clkBuf.OB
 
-  // --- Command/Address/Control Pins (Write Path) ---
   for (i <- 0 until sl.chipAddressWidth) io.sdram.ADDR(i) := valueToOutput("ADDR", RegNext(io.ctrl.ADDR(i)))
-  for (i <- 0 until sl.bankWidth) io.sdram.BA(i) := valueToOutput("BA", RegNext(io.ctrl.BA(i)))
+  for (i <- 0 until sl.bankWidth)        io.sdram.BA(i)   := valueToOutput("BA", RegNext(io.ctrl.BA(i)))
   io.sdram.CASn := sdrToOutput("CASn", io.ctrl.phases.map(p => RegNext(p.CASn)))
-  io.sdram.CKE := sdrToOutput("CKE", io.ctrl.phases.map(p => RegNext(p.CKE)))
-  io.sdram.CSn := sdrToOutput("CSn", io.ctrl.phases.map(p => RegNext(p.CSn)))
+  io.sdram.CKE  := sdrToOutput("CKE",  io.ctrl.phases.map(p => RegNext(p.CKE)))
+  io.sdram.CSn  := sdrToOutput("CSn",  io.ctrl.phases.map(p => RegNext(p.CSn)))
   io.sdram.RASn := sdrToOutput("RASn", io.ctrl.phases.map(p => RegNext(p.RASn)))
-  io.sdram.WEn := sdrToOutput("WEn", io.ctrl.phases.map(p => RegNext(p.WEn)))
+  io.sdram.WEn  := sdrToOutput("WEn",  io.ctrl.phases.map(p => RegNext(p.WEn)))
   if (sl.generation.RESETn) io.sdram.RESETn := sdrToOutput("RESETn", io.ctrl.phases.map(p => RegNext(p.RESETn)))
   io.sdram.ODT := sdrToOutput("ODT", io.ctrl.phases.map(p => RegNext(p.ODT)))
 
-  val idelayValueIn = in Bits (9 bits)
+  val idelayValueIn = in Bits(9 bits) // UltraScale: 9 bits
 
-  val dqe0Reg = RegNext(io.ctrl.writeEnable) init (False)
-  val dqe270Reg = clk270(RegNext(io.ctrl.writeEnable) init (False))
+  val dqe0Reg = RegNext(io.ctrl.writeEnable) init(False)
+  val dqe270Reg = clk270(RegNext(io.ctrl.writeEnable) init(False))
   val dqstReg = clk270(RegNext(Vec((io.ctrl.writeEnable ## dqe270Reg).mux(
     B"00" -> B"0000",
     B"10" -> B"0011",
     B"11" -> B"1111",
-    B"01" -> B"1100"
+    B"01" -> B"1111"
   ).asBools.reverse)))
   dqstReg.foreach(_.init(False))
-  io.debug.dqsEnableWindow := B(dqstReg)
 
   val dqReg = io.ctrl.phases.map(p => RegNext(p.DQw))
   val dmReg = io.ctrl.phases.map(p => RegNext(p.DM))
 
-  // --- DQS Write Path ---
-  val dqs = for (i <- 0 until (sl.dataWidth + 7) / 8) yield new Area {
-    val (serQ, serT) = seqToOutput("CK", Seq.fill(phaseCount)(Seq(True, False)).flatten, dqstReg, phase90 = true)
+  val dqs = for (i <- 0 until (sl.dataWidth+7)/8) yield new Area {
+    val (serQ, serT) = seqToOutput("DQS", Seq.fill(phaseCount)(Seq(True, False)).flatten, dqstReg, phase90 = true)
     val buf = IOBUFDS()
     buf.T := serT
     buf.I := serQ
-    io.debug.dqsT(i) := serT
-    io.sdram.DQS(i) := buf.IO
+    io.sdram.DQS(i)  := buf.IO
     io.sdram.DQSn(i) := buf.IOB
   }
 
-  // --- DM Write Path ---
-  val dm = for (i <- 0 until (sl.dataWidth + 7) / 8) yield new Area {
+  val dm = for (i <- 0 until (sl.dataWidth+7)/8) yield new Area {
     io.sdram.DM(i) := ddrToOutput("DM", dmReg.map(_.map(_(i))))
   }
 
   io.ctrl.readValid := io.ctrl.readEnable
 
-  // --- DQ Write Path ---
-  val dqTVec = Vec(Bool, sl.dataWidth)
   val dq = for (i <- 0 until sl.dataWidth) yield new Area {
     val buf = IOBUF()
     io.sdram.DQ(i) := buf.IO
@@ -163,29 +135,125 @@ case class XilinxUSPhy(sl : SdramLayout,
     val (serQ, serT) = seqToOutput("DQ", dqReg.map(_.map(_(i))).flatten, List.fill(4)(dqe0Reg))
     buf.T := serT
     buf.I := serQ
-    dqTVec(i) := buf.T
 
-    // --- Read Path (unimplemented) ---
+    val idelay = IDELAYE3(DELAY_TYPE = "VAR_LOAD")
+    idelay.CLK         := serdesClk0.readClockWire
+    idelay.IDATAIN     := buf.O
+    idelay.DATAIN      := False
+    idelay.CE          := False
+    idelay.INC         := True
+    idelay.LOAD        := in Bool()
+    idelay.CNTVALUEIN  := idelayValueIn
+    idelay.RST         := False
+    idelay.EN_VTC      := True
+
+    val des = ISERDESE3(DATA_WIDTH = phaseCount*2)
+    des.CLK    := serdesClk0.readClockWire
+    des.CLK_B  := !serdesClk0.readClockWire
+    des.CLKDIV := ClockDomain.current.readClockWire
+    des.RST    := ClockDomain.current.isResetActive
+    des.D      := idelay.DATAOUT
+
+    for (phase <- 0 until phaseCount; ratio <- 0 until pl.dataRate) {
+      io.ctrl.phases(phase).DQr(ratio)(i) := des.Q((phaseCount*pl.dataRate-1) - (phase*pl.dataRate + ratio))
+    }
   }
-  io.debug.dqT := B(dqTVec)
 
-
-  case class PLLE2_ADV() extends BlackBox {
-    addGeneric("CLKIN1_PERIOD", 10.0)
-    addGeneric("CLKFBOUT_MULT", 8)
-    addGeneric("CLKOUT0_DIVIDE", 4)
-    addGeneric("CLKOUT0_PHASE", 0)
-    addGeneric("CLKOUT1_DIVIDE", 2)
-    addGeneric("CLKOUT1_PHASE", 90)
-
-    val CLKFBIN = in Bool()
-    val CLKIN1 = in Bool()
-    val CLKFBOUT = out Bool()
-    val CLKOUT0 = out Bool()
-    val CLKOUT1 = out Bool()
+  def driveFrom(mapper: BusSlaveFactory): Unit = {
+    mapper.drive(idelayValueIn, 0x00)
+    mapper.driveMultiWord(Vec(dq.map(_.idelay.LOAD)), 0x20)
+    mapper.driveMultiWord(Vec(dq.map(_ => False)), 0x40)
   }
 }
 
-//sl = SdramLayout(dataWidth = 8, generation = DDR3, bankWidth = 3, columnWidth = 10, rowWidth = 14)
 
+
+//object USPhyWriteTest extends App {
+//
+//  class USPhyWriteTop extends Component {
+//    val io = new Bundle {
+//      val clkIn = in Bool() // Single reference clock (e.g., 100 MHz)
+//      val reset = in Bool()
+//    }
+//
+//    // Example DDR3 SDRAM layout
+//    val sdramLayout = SdramLayout(
+//      generation = SdramGeneration.DDR3,
+//      dataWidth = 8,              // 8-bit DQ
+//      columnWidth = 14,
+//      bankWidth = 3,
+//      rowWidth = 8
+//    )
+//
+//    // Clock generator for UltraScale (similar to S7's PLLE2_ADV)
+//    val mmcm = MMCME3_ADV(
+//      CLKFBOUT_MULT_F = 8.0,
+//      CLKIN1_PERIOD = 10.0,     // 100 MHz input
+//      CLKOUT0_DIVIDE_F = 4.0,   // Parallel clock
+//      CLKOUT1_DIVIDE = 4,       // 90° phase shift
+//      CLKOUT1_PHASE = 90.0
+//    )
+//
+//    mmcm.CLKFBIN := mmcm.CLKFBOUT
+//    mmcm.CLKIN1 := io.clkIn
+//    mmcm.RST := io.reset
+//
+//    // Create clock domains
+//    val serdesClk0 = ClockDomain(mmcm.CLKOUT0, io.reset)
+//    val serdesClk90 = ClockDomain(mmcm.CLKOUT1, io.reset)
+//
+//    // In UltraScale, clk90 for control can be same as serdesClk90
+//    val phy = XilinxUSPhy(
+//      sl = sdramLayout,
+//      clkRatio = 2,
+//      clk90 = serdesClk90,
+//      serdesClk0 = serdesClk0,
+//      serdesClk90 = serdesClk90
+//    )
+//
+//    // Drive a counter pattern into write data
+//    val counterArea = new ClockingArea(serdesClk0) {
+//      val counter = Reg(UInt(8 bits)) init(0)
+//      counter := counter + 1
+//
+//      phy.io.ctrl.phases(0).WEn := True
+//      for (bit <- 0 until 8) {
+//        phy.io.ctrl.phases(0).DQw(0)(bit) := counter(bit)
+//      }
+//    }
+//
+//    // Tie off unused
+//    for (i <- 0 until phy.io.ctrl.ADDR.getWidth)
+//      phy.io.ctrl.ADDR(i) := False
+//    for (phase <- phy.io.ctrl.phases) {
+//      for (dm <- phase.DM) {  // DM is probably a Vec of Bits
+//        for (i <- 0 until dm.getWidth)
+//          dm(i) := False
+//      }
+//      phase.WEn := False
+//    }
+//  }
+//
+//  // Simulation config
+//  SimConfig.withWave.compile(new USPhyWriteTop).doSim { dut =>
+//    dut.io.reset #= true
+//    dut.io.clkIn #= false
+//
+//    // Generate single reference clock
+//    fork {
+//      while (true) {
+//        dut.io.clkIn #= false
+//        sleep(5)
+//        dut.io.clkIn #= true
+//        sleep(5)
+//      }
+//    }
+//
+//    sleep(20)
+//    dut.io.reset #= false
+//
+//    // Run for some cycles to see DQ/DQS toggling
+//    sleep(500)
+//  }
+//}
 
